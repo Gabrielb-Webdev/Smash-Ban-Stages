37f557c439a0327bcb053c6c1446de40f5d65899
Gabriel Bustos <49459453+Gabrielb-Webdev@users.noreply.github.com>
2026-03-07 13:13:28 -0300

    Actualizar constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
 // Constants para el sistema de baneos
-// Orden específico para Game 1: Small Battlefield, Town and City, Pokemon Stadium 2, Hollow Bastion, Battlefield
+// Orden específico para Game 1: Small Battlefield, Town and City, Pokemon Stadium 2, Smashville, Battlefield
 export const STAGES_GAME1 = [
   { id: 'small-battlefield', name: 'Small Battlefield', image: '/images/stages/Small Battlefield.png' },
   { id: 'town-and-city', name: 'Town and City', image: '/images/stages/Town and City.png' },
   { id: 'pokemon-stadium-2', name: 'Pokémon Stadium 2', image: '/images/stages/Pokemon Stadium 2.png' },
-  { id: 'hollow-bastion', name: 'Hollow Bastion', image: '/images/stages/Hollow Bastion.png' },
+  { id: 'Smashville', name: 'Smashville', image: '/images/stages/Smashville.png' },
   { id: 'battlefield', name: 'Battlefield', image: '/images/stages/Battlefield.png' },
 ];
 
